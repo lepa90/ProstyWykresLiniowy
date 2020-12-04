d2582fa96fcb3986b88499266ffaf38f19b52fae
kamil-dev <kamil.przylepa@gmail.com>
2020-12-04 15:40:03 +0100

    04.12.20

--- prosty_wykres_liniowy.py
+++ prosty_wykres_liniowy.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+plt.style.use('seabornk')
+
 fig, ax =plt.subplots()
-ax.plot(squares, linewidth=5)
+ax.plot(input_values, squares, linewidth=5)
 ax.set_title("Kwadraty liczb", fontsize=24)
 ax.set_xlabel("Wartość", fontsize=14)
 ax.set_ylabel("Kwadraty wartości", fontsize=14)
