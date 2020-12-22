d1437dd42f2a527dca6d439ca55f405bbd263cc2
kamil-dev <kamil.przylepa@gmail.com>
2020-12-22 12:43:48 +0100

    22.12.2020

--- rw_visual.py
+++ rw_visual.py
@@ -1,11 +1,20 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
 
-plt.style.use('classic')
+    plt.style.use('classic')
 
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    fig, ax = plt.subplots(figsize=(15,9))
+    point_number = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_number, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.show()
+    keep_running = input('Utworzyc kolejne bladzenie losowe? (t/n): ')
+    if keep_running == 'n':
+        break
